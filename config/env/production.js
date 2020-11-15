219d5864b9a6ec77fabfbb57c10834b28b958daf
Jason Lincoln <jlincoln@gandalf.local>
2020-11-15 14:43:32 -0600

    enable csrf again, set baseUrl to https version, addded to origins list

--- config/env/production.js
+++ config/env/production.js
@@ -251,6 +251,8 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     onlyAllowOrigins: [
+      'http://www.elephantchess.net',
+      'https://www.elephantchess.net',
       'http://nodetest1-env.eba-cix5yirt.us-east-1.elasticbeanstalk.com',
     //   'https://example.com',
     //   'https://staging.example.com',
@@ -374,7 +376,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'http://www.elephantchess.net/',
+    baseUrl: 'https://www.elephantchess.net/',
     internalEmailAddress: 'support@example.com',
 
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
