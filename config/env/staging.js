136bd5fe0b9293cccaddb1bb4e3400e8dd2a0038
Jason Lincoln <jlincoln@gandalf.local>
2020-11-19 09:10:34 -0600

    setting migrate to alter for staging

--- config/env/staging.js
+++ config/env/staging.js
@@ -39,6 +39,10 @@ module.exports = Object.assign({}, PRODUCTION_CONFIG, {
     })
   }),
 
+  models: {
+    migrate: 'alter',
+  },
+
   sockets: Object.assign({}, PRODUCTION_CONFIG.sockets, {
 
     onlyAllowOrigins: [
